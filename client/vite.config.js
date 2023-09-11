b5346cc485bfa0bfed2f1a0c371759481b89e62a
AuraHuang <aurahuang1996@gmail.com>
2023-09-11 15:00:57 +0800

    update api proxy setting

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,9 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-           target: 'http://localhost:3000',
-           changeOrigin: true,
-           secure: false,      
+          // localhost:3000 
+          target: 'http://booking-web-backend.vercel.app',
+          changeOrigin: true,
+          secure: false,      
        }
     }
   }
